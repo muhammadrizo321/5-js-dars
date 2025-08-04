11ac37714bf796fadc1679d77d4e3ae24bec38d6
Muhammadrizo <rahmatullayevmuhammadrizo888>
2025-08-04 17:41:59 +0500

    uploaded homework

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// let nuber1 = "Sarvar";
+// let age = "55"
+// let kaspi = "duradgor"
+// alert()
+// let nuber2 = "Anvar";
+// let age2 = "22";
+// let kaspi2 = "usta";
+
+// let nuber3 = "Oqil";
+// let age3 = "18";
+// let kaspi3 = "oqtuvchi";
+
+// let nuber4 = "Husan";
+// let age4 = " 17"
+// let kaspi4 = "33"
+
+// let nuber5 = "Hasan";
+// let age5 = "33";
+// let kaspi5 = "krakash";
+
+
+let num =prompt("soz kiriting")
+const user = {
+    age:55,
+    job:66,
+    name: "sanvar"
+}
+
+
+
+
